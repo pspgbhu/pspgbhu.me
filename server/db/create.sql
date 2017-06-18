4e3ec97af7a3a15c9d65f4329f7c2d86951dedf8
hucong <hucong@jd.com>
2017-06-18 23:58:42 +0800

    update

--- server/db/create.sql
+++ server/db/create.sql
@@ -11,5 +11,5 @@ CREATE TABLE `blog`.`essay` (
 	PRIMARY KEY (`id`),
 	INDEX `id` USING BTREE (id),
 	INDEX `views` USING BTREE (views),
-	INDEX `tags` USING BTREE (tags)
+	INDEX `categories` USING BTREE (categories)
 ) ENGINE=`InnoDB` AUTO_INCREMENT=1 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ROW_FORMAT=DYNAMIC CHECKSUM=0 DELAY_KEY_WRITE=0;
