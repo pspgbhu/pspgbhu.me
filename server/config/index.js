125606aad57c4e82c84bb565b99bd31b39c7c513
hucong <hucong@jd.com>
2017-06-19 01:06:37 +0800

    updatae

--- server/config/index.js
+++ server/config/index.js
@@ -2,6 +2,6 @@ process.env.NODE_ENV = process.env.npm_lifecycle_event === 'start'
   ? 'production'
   : 'development';
 
-const status = process.env.NODE_ENV === 'produciton' ? 'prod' : 'dev';
+const status = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
 const config = `./config.${status}.js`;
 module.exports = require(config);
